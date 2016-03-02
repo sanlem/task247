13c212774c4e74b7b24e2a835d1380612a7837ec
Sanlem <vfranchook@gmail.com>
2016-03-02 13:51:03 +0200

    switched to postgres

--- task247/settings.py
+++ task247/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'task247.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'task247',
+        'USER': 'taskadmin',
+        'PASSWORD': 'zX1_bQr',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
