700dcdfaa8c62afae35074f0094dc2fe8c0db0ef
Sanlem <vfranchook@gmail.com>
2016-03-12 00:11:56 +0200

    attachments now can be related with tor or with ticket

--- tickets/models.py
+++ tickets/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from projects.models import Project
+from projects.models import Project, TOR
 from django.conf import settings
 import os
 
@@ -66,14 +66,30 @@ class TicketComment(models.Model):
 
 def upload_path_generator(instance, filename):
     """ files should be saved to the ticket's folder inside project's folder. """
-    ticket = instance.ticket
-    return '%s/%s/%s' % (ticket.project.name, ticket.name, filename)
+    if instance.ticket:
+        ticket = instance.ticket
+        return '%s/tickets/%s/%s' % (ticket.project.name, ticket.name, filename)
+    else:
+        tor = instance.tor
+        return '%s/tors/%s/%s' % (tor.project.name, str(tor), filename)
 
 class Attachment(models.Model):
     attachment = models.FileField(upload_to=upload_path_generator)
-    ticket = models.ForeignKey(Ticket)
+    ticket = models.ForeignKey(Ticket, null=True, blank=True)
+    tor = models.ForeignKey(TOR, null=True, blank=True)
     name = models.CharField(max_length=30)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
 
+    def save(self, *args, **kwargs):
+        if (not self.tor and not self.ticket):
+            raise ValueError('Attachment should have TOR or ticket')
+        if self.tor and self.ticket:
+            raise ValueError('Attachment can have only TOR or only Ticket, not both')
+        super (Attachment, self).save(*args, **kwargs)
+
+
     def __str__(self):
-        return '%s#%s - %s' % (self.ticket.project.name, self.ticket.name, self.name)
+        if self.ticket:
+            return '%s#%s-%s' % (self.ticket.project.name, self.ticket.name, self.name)
+        else:
+            return '%s#%s-%s' % (self.tor.project.name, str(self.tor), self.name)
