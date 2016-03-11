1e1a492715283a796d709c2b06e078d8d625b2e7
Sanlem <vfranchook@gmail.com>
2016-03-11 19:16:16 +0200

    attachments are now loaded inside ticket folder which is inside project folder

--- tickets/models.py
+++ tickets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from projects.models import Project
+from django.conf import settings
+import os
 
 
 class Ticket(models.Model):
@@ -62,8 +64,16 @@ class TicketComment(models.Model):
     ticket = models.ForeignKey(Ticket)
 
 
+def upload_path_generator(instance, filename):
+    """ files should be saved to the ticket's folder inside project's folder. """
+    ticket = instance.ticket
+    return '%s/%s/%s' % (ticket.project.name, ticket.name, filename)
+
 class Attachment(models.Model):
-    attachment = models.FileField(upload_to='documents/%Y/%m/%d')
+    attachment = models.FileField(upload_to=upload_path_generator)
     ticket = models.ForeignKey(Ticket)
     name = models.CharField(max_length=30)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return '%s#%s - %s' % (self.ticket.project.name, self.ticket.name, self.name)
