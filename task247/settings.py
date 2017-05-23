1f4175dcc6b2ce08f1f253b9095ebe8227d55f36
Vitaliy Franchook <vfranchook@gmail.com>
2017-05-23 22:24:46 +0300

    added static_root

--- task247/settings.py
+++ task247/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 LOGIN_REDIRECT_URL = '/'
 
 REST_FRAMEWORK = {
